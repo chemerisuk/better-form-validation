0f5d05b208033c84db34defafd3188691e46b46d
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-18 21:32:54 +0300

    Update 2014-10-18T18:32:54.827Z

---
+++ i18n/better-form-validation.ru.js
@@ -0,0 +1,8 @@
+DOM.importStrings("ru", {
+    "can't be empty": "не может быть пустым",
+    "illegal value format": "недопустимый формат",
+    "should be a valid URL": "должен быть правильный URL",
+    "should be a valid email": "должен быть правильный email",
+    "should be a valid phone number": "должен быть номер телефона",
+    "should be a numeric value": "должно быть числовое значение"
+});
