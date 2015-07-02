202b67f4a25fef011fc0555112a825f7d6a9bf2e
Muhammad Syafiq Zainuddin <syafiq@my-sands.com>
2015-07-02 21:30:51 +0800

    add Malay (Malaysia) lang support

---
+++ i18n/better-form-validation.ms.js
@@ -0,0 +1,8 @@
+DOM.importStrings("ms", {
+    "field is required": "medan diperlukan",
+    "illegal value format": "format data tidak sah" ,
+    "should be a valid URL": "pastikan URL ini sah",
+    "should be a valid email": "pastikan emel ini sah",
+    "should be a valid phone number": "pastikan nombor telefon ini sah",
+    "should be a numeric value": "pastikan nombor ini sah"
+});
