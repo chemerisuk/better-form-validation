dcedd1ca2874d48c7077bf68d086545d89aaae42
Yuka Kato <y-kato@esm.co.jp>
2015-09-05 14:09:54 +0900

    Add Japanese language support

---
+++ i18n/better-form-validation.ja.js
@@ -0,0 +1,8 @@
+DOM.importStrings("ja", {
+    "field is required": "必ず入力してください",
+    "illegal value format": "不正な形式です" ,
+    "should be a valid URL": "URL として正しいものを入力してください",
+    "should be a valid email": "メールアドレスとして正しいものを入力してください",
+    "should be a valid phone number": "電話番号として正しいものを入力してください",
+    "should be a numeric value": "数字を入力してください"
+});
